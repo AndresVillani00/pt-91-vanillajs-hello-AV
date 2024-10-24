910aa88bac81e82ab35b1d2e6de25ac24b54b165
AndresVillani00 <73246809+AndresVillani00@users.noreply.github.com>
2024-10-24 17:10:08 +0000

    Primer commit para el generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+  let action = [" ate", " peed", " crushed", " broke"];
+  let what = [" my homework ", " my phone ", " the car "];
+  let when = [
+    " before the class",
+    " when I was sleeping",
+    " while I was exercising",
+    " during my lunch",
+    " while I was praying"
+  ];
+
+  function excusaAleatoria() {
+    let excusaWho = Math.floor(Math.random() * who.length);
+    let excusaAction = Math.floor(Math.random() * action.length);
+    let excusaWhat = Math.floor(Math.random() * what.length);
+    let excusaWhen = Math.floor(Math.random() * when.length);
+
+    let excusa =
+      who[excusaWho] +
+      action[excusaAction] +
+      what[excusaWhat] +
+      when[excusaWhen];
+    document.getElementById("excusas").innerHTML = "<h3>" + excusa + "</h3>";
+  }
+
+  excusaAleatoria();
 };
